64908ea917387756c1e422aec6d3f040e94c1908
cwha0212 <you@example.com>
2022-10-02 08:20:59 +0900

    pi0 수정, 정리

--- pi0/main.py
+++ pi0/main.py
@@ -1,27 +1,23 @@
 import cam_controler
+import gps_test
+import time
 
-def PrintResult(result):
-  for i in range(0,len(result)):
-    cam_num = i+1
-    if result[i] == 1:
-      print(f"cam{cam_num} connect failed")
-    elif result[i] == 2:
-      print(f"cam{cam_num} message send failed")
-    elif result[i] == 3:
-      print(f"cam{cam_num} complete")
-    else:
-      print(f"cam{cam_num} unknown error")
+cam1_IP = '192.168.0.7'
+cam2_IP = '192.168.0.9'
+cam3_IP = '192.168.0.10'
 
-
-CamControler = cam_controler.CamControler()
+CamControler = cam_controler.CamControler(cam1_IP, cam2_IP, cam3_IP)
 result = CamControler.open(0)
-PrintResult(result)
 
 while True:
   for i in range(0,3):
     if result[i] != 3:
       result = CamControler.open(i+1)
-      PrintResult(result)
   
   if result == [3,3,3]:
     break
+
+a = gps_test.gpsTransmit()
+for i in range(0,10):
+  a.gpsTransmit()
+  time.sleep(1)
