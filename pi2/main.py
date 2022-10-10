c66950b6070b502f3e0ce0f9b328314d48342b3d
cwha0212 <cwha02120212@gmail.com>
2022-10-10 16:15:56 +0900

    pi2

--- pi2/main.py
+++ pi2/main.py
@@ -8,9 +8,15 @@ from firebase_admin import firestore
 UV4L_ON = "sudo service uv4l_raspicam restart"
 UV4L_OFF = "sudo pkill uv4l"
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("google.com",443))
-IP2 = sock.getsockname()[0]
+while True:
+  ipaddress=socket.gethostbyname(socket.gethostname())
+  if ipaddress=="127.0.0.1":
+    continue
+  else:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("google.com",443))
+    IP2 = sock.getsockname()[0]
+    break
 
 cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
@@ -23,7 +29,7 @@ doc_ref.set({
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('192.168.0.13', 9000))
+server_socket.bind((IP2, 9000))
 server_socket.listen(10)
 
 while True:
